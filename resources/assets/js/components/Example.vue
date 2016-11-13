54979b34498af7fccf81619ede311840ae58e5bc
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-13 19:45:45 +0700

    first commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                users: {}
+            }
+        }
+        created() {
+            $.getJSON('http://api.viender.dev' + newNoSppc, function(data) {
+                alert(data);
+            }->bind(this));
+        }
+    }
+</script>
