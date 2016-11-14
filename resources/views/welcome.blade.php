58d75eea24e92b287ffc680aa3769f6776bfb758
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-14 12:54:13 +0700

    add welcome page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app" class="flex-center position-ref full-height">
+            <answer-list></answer-list>
+        </div>
+        <script src="{{ elixir('js/app.js') }}"></script>
+    </body>
+</html>
