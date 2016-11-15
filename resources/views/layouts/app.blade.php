4c1ea2e6595d6083c671fcea36304548c399e95e
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-15 12:24:02 +0700

    rebuild

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,64 +18,23 @@
 </head>
 
 <body>
+    <div id="root">
+        <header>
+            <!--Navbar-->
+                @include('layouts.nav')
+            <!--/.Navbar-->
+        </header>
 
-    <main id="app">
-        <!--Navbar-->
-        <nav class="navbar navbar-dark bg-primary">
-            <!--Collapse button-->
-            <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="fa fa-bars white-text"></i></a>
+        <main id="app">
+            
+        </main>
 
-            <!--Content for large and medium screens-->
-            <div class="navbar-desktop">
-                <!--Navbar Brand-->
-                <a class="navbar-brand" href="#">Navbar</a>
-                <!--Links-->
-                <ul class="nav navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                </ul>
-                <!--Search form-->
-                <form class="form-inline float-xs-right">
-                    <input class="form-control" type="text" placeholder="Search">
-                </form>
-            </div>
-
-            <!-- Content for mobile devices-->
-            <div class="navbar-mobile">
-                <ul class="side-nav" id="mobile-menu">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <!--/.Navbar-->
-    </main>
-
-
-
+        <footer>
+            
+        </footer>
+    </div>
+    
     <!-- SCRIPTS -->
-
-    <!-- JQuery -->
     <script type="text/javascript" src={{ elixir("js/app.js") }}></script>
 
     <script>
