63333c0779361043878e17a767e9b70099e324e5
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-17 14:28:04 +0700

    move mobile menu button

--- resources/views/layouts/navigation/navbar-mobile.blade.php
+++ resources/views/layouts/navigation/navbar-mobile.blade.php
@@ -1,5 +1,11 @@
 <div class="navbar-mobile">
-    <ul class="nav nav-tabs tabs-3 bg-primary" id="mobile-menu-head">
+    <ul class="nav nav-tabs tabs-4 bg-primary" id="mobile-menu-head">
+        <!--Collapse button-->
+        <li>
+            <div  class="nav-item waves-effect waves-light">
+                <span><a href="#" data-activates="mobile-menu" class="button-collapse" id="mobile-menu-button" style="text-align: left;"><i class="fa fa-bars white-text"></i></a></span>
+            </div>
+        </li>
         @include('layouts.navigation.navbar-items')
     </ul>
     <ul class="side-nav blue-grey lighten-5" id="mobile-menu">
--- resources/views/layouts/navigation/navigation.blade.php
+++ resources/views/layouts/navigation/navigation.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-dark bg-primary">
     <div class="row">    
-        <!--Collapse button-->
-        <a href="#" data-activates="mobile-menu" class="button-collapse" id="mobile-menu-button"><i class="fa fa-bars white-text"></i></a>
+       
     </div>
 
     <!--Content for large and medium screens-->
