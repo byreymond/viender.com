10c588c0ba96a0370060c79dc73312c7d347b700
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-18 08:47:52 +0700

    new data is appended to old data

--- resources/assets/js/components/answers/AnswerList.vue
+++ resources/assets/js/components/answers/AnswerList.vue
@@ -3,6 +3,7 @@
         <div  class="answer-box" v-for="answer in answers.data">
             <answer :answer="answer"></answer>
         </div>
+        <button type="button" class="btn btn-outline-primary waves-effect" style="width: 90%; margin: auto; display: block;" @click="fetchAnswers()">Load More Answers</button>
     </div>
 </template>
 
@@ -12,27 +13,35 @@
 
         data() {
             return {
-                answers: {},
-            }
-        },
+                answers: {
+                    data: []
+                },
 
-        created() {
-            this.getData();
-        },
-
-        methods: {
-            getData() {
-                var pathParams = {
+                answersPathParams: {
                     params: {
                         with: "owner,question",
                         page: 1
                     }
-                };
+                }
+            }
+        },
 
-                axios.get('/answers', pathParams)
+        created() {
+            this.fetchAnswers();
+        },
+
+        methods: {
+            fetchAnswers() {
+                var vm = this;
+
+                axios.get('/answers', vm.answersPathParams)
                     .then(function (response) {
-                        this.answers = response.data;
-                    }.bind(this))
+                        response.data.data.forEach(function(answer) {
+                            vm.answers.data.push(answer);
+                        });
+
+                        vm.answersPathParams.params.page += 1;
+                    })
                     .catch(function (error) {
                         console.log(error);
                     });
