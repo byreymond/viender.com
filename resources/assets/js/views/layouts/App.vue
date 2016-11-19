8dc7085fa1469faee907e15aa70397b94e29f286
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-19 16:14:13 +0700

    Add App.vue

---
+++ resources/assets/js/views/layouts/App.vue
@@ -0,0 +1,21 @@
+<template id="app-template">
+    <div class="app">
+        <header>
+            <slot name="header"></slot>
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        template: "#app-template",
+    }
+</script>
