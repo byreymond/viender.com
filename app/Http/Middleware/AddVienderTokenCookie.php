0e27df77ceaa1e818af943a75665535e76c37480
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-23 18:12:52 +0700

    Add cokie to every web request

--- app/Http/Middleware/AddVienderTokenCookie.php
+++ app/Http/Middleware/AddVienderTokenCookie.php
@@ -17,11 +17,14 @@ class AddVienderTokenCookie
     {
         $response = $next($request);
         
-        if ($request->session()->has('viender_access_token')) {
-            if(! $request->hasCookie('viender_access_token'))
+        if ($request->session()->has('viender_access_token')
+            && ! $request->hasCookie('viender_access_token')
+            && ! $request->hasCookie('viender_access_token_expires_at')
+            && ! $response instanceof \Symfony\Component\HttpFoundation\RedirectResponse)
             {
-                return $response->cookie('viender_access_token', session('viender_access_token'));
-            }
+                $response->cookie('viender_access_token', session('viender_access_token'), 60*24*7, null, null, false, false);
+                $response->cookie('viender_access_token_expires_at', session('viender_access_token_expires_at'), 60*24*7, null, null, false, false);
+                return $response;
         }
 
         return $response;
