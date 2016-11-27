09c8cad265007ac300c568f644ba60600dce48eb
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-27 11:46:51 +0700

    Add question show page

---
+++ resources/assets/js/components/pages/QuestionShowPage.vue
@@ -0,0 +1,15 @@
+<template id="question-show-page-template">
+    <app class="read-page">
+        <navigation slot="header"></navigation>
+        <answer-list url="/answers"></answer-list>
+    </app>
+</template>
+
+<script>
+    export default {
+        template: "#question-show-page-template",
+
+        created() {
+        }
+    }
+</script>
