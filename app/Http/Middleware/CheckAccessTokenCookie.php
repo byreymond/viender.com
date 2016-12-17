79eb520a8f2458b406aca7d6273283aaec4de54e
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-12-18 03:35:18 +0700

    Token refresher

--- app/Http/Middleware/CheckAccessTokenCookie.php
+++ app/Http/Middleware/CheckAccessTokenCookie.php
@@ -3,27 +3,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Viender\TokenRefresher;
 use Illuminate\Support\Facades\Auth;
 
 class CheckAccessTokenCookie
 {
-    protected $header;
-
-    protected $accessTokenCookieName;
-
-    protected $refreshTokenCookieName;
+    protected $tokenRefresher;
 
 
-    public function __construct() {
-        $this->header = [
-            'Origin'            => config('app.url'),
-            'Content-Type'      => 'application/json',
-            'Accept'            => 'application/json'
-        ];
-
-        $this->accessTokenCookieName = config('services.viender.access_token_cookie_name');
-
-        $this->refreshTokenCookieName = config('services.viender.refresh_token_cookie_name');
+    public function __construct(TokenRefresher $tokenRefresher) {
+        $this->tokenRefresher = $tokenRefresher;
     }
 
     /**
@@ -38,46 +27,16 @@ class CheckAccessTokenCookie
     {
         $response = $next($request);
 
-        if( ! $request->cookie($this->accessTokenCookieName)) {
+        if( ! $request->cookie(config('services.viender.access_token_cookie_name'))) {
             // if no secret cookie, try to get new one
-            if( ! $newSecret = $this->getNewToken($request)) {
+            if( ! $newSecret = $this->tokenRefresher->getNewToken($request)) {
                 // if cannot get new token redirect to home page
                 return redirect(url('/welcome'));
             } else {
-                $response->cookie($this->accessTokenCookieName, $newSecret['token_type'] . ' ' . $newSecret['access_token'], $newSecret['expires_in']/60, null, config('app.domain'));
-                $response->cookie($this->refreshTokenCookieName, $newSecret['refresh_token'], 60*24*360, null, config('app.domain'));
+                $this->tokenRefresher->setTokenCookie($response);
             }
         }
 
         return $response;
-    }
-
-    private function getNewToken($request)
-    {
-        $secret = null;
-
-        if( $refreshToken = $request->cookie($this->refreshTokenCookieName)) {
-            $http = new \GuzzleHttp\Client(['base_uri' => config('services.viender.url')]);
-
-            if(config('app.env') == 'local') {
-                $http = new \GuzzleHttp\Client(['base_uri' => config('services.viender.url'), 'verify' => false]);
-            }
-
-            $response = $http->post('/oauth/token', [
-                'headers' => $this->header,
-                'json' => [
-                    'grant_type'        => 'refresh_token',
-                    'client_id'         => config('services.viender.client_id'),
-                    'client_secret'     => config('services.viender.client_secret'),
-                    'refresh_token'     => $refreshToken,
-                    'scope'             => '',
-                ],
-            ]);
-
-            $secret = json_decode((string) $response->getBody(), true);
-        }
-
-
-        return $secret;
     }
 }
